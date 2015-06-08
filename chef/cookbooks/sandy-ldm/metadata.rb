c68a8adb9d4fd94c173d74f56c79a5379594ecd1
Scott Macfarlane <scott@gina.alaska.edu>
2015-06-08 10:32:03 -0800

    Update ldm config

--- chef/cookbooks/sandy-ldm/metadata.rb
+++ chef/cookbooks/sandy-ldm/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license          'all_rights'
 description      'Installs/Configures sandy-ldm'
 long_description 'Installs/Configures sandy-ldm'
-version          '0.3.0'
+version          '0.4.0'
 
 depends 'ldm'
 depends 'nginx'
