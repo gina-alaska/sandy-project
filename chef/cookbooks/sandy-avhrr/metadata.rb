35fa2f13a53aa73effeeab2f83c6cb0c9b8853f0
Scott Macfarlane <scott@gina.alaska.edu>
2015-07-22 09:22:14 -0800

    Add NFS cookbook

--- chef/cookbooks/sandy-avhrr/metadata.rb
+++ chef/cookbooks/sandy-avhrr/metadata.rb
@@ -4,6 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures sandy-avhrr'
 long_description 'Installs/Configures sandy-avhrr'
-version '0.s.0'
+version '0.4.0'
 
 depends 'polar2grid'
+depends 'nfs'
--- chef/cookbooks/sandy-avhrr/recipes/default.rb
+++ chef/cookbooks/sandy-avhrr/recipes/default.rb
@@ -5,6 +5,7 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 include_recipe "polar2grid"
+include_recipe "nfs"
 
 directory "/opt/terascan" do
   recursive true
